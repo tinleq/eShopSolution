27bba1dbb69fad4ca9aa4a32e6392d616987705f
LE TIN <tinleq95@outlook.com>
2020-11-16 15:31:16 +0700

    Add Entity

---
+++ eShopSolution.Data/Entities/CategoryTranslation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class CategoryTranslation
+    {
+        public int Id { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string SeoDescription { get; set; }
+
+        public string SeoTitlte { get; set; }
+
+        public string LanguageId { get; set; }
+
+        public string SeoAlias { get; set; }
+
+        public Category Category { get; set; }
+
+        public Language Language { get; set; }
+    }
+}
