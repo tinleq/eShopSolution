2ad515187323288f6d48def367d3f2af354a9b21
LE TIN <tinleq95@outlook.com>
2020-11-18 17:57:11 +0700

    swagger login

--- eShopSolution.BackendApi/Startup.cs
+++ eShopSolution.BackendApi/Startup.cs
@@ -1,10 +1,13 @@
 using eShopSolution.Application.Catalog.Products;
 using eShopSolution.Application.Common;
+using eShopSolution.Application.System.Users;
 using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
 using eShopSolution.Ultilities.Constants;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,15 +27,51 @@ namespace eShopSolution.BackendApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddDbContext<eShopDbContext>(options =>
+        //        options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
+
+        //    services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<eShopDbContext>().AddDefaultTokenProviders();
+
+        //    services.AddTransient<IStorageService, FileStorageService>();
+
+        //    services.AddTransient<IPublicProductService, PublicProductService>();
+
+        //    services.AddTransient<IManageProductService, ManageProductService>();
+
+        //    services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+
+        //    services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+
+        //    services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+
+        //    services.AddTransient<IUserService, UserService>();
+        //    services.AddControllersWithViews();
+
+        //    services.AddSwaggerGen(c =>
+        //    {
+        //        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger eShop Solution", Version = "v1" });
+        //    });
+        //}
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<eShopDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
+
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<eShopDbContext>()
+                .AddDefaultTokenProviders();
+
+
             services.AddTransient<IStorageService, FileStorageService>();
 
             services.AddTransient<IPublicProductService, PublicProductService>();
-
             services.AddTransient<IManageProductService, ManageProductService>();
+            services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+            services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+            services.AddTransient<IUserService, UserService>();
 
             services.AddControllersWithViews();
 
@@ -43,6 +82,40 @@ namespace eShopSolution.BackendApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        //{
+        //    if (env.IsDevelopment())
+        //    {
+        //        app.UseDeveloperExceptionPage();
+        //    }
+        //    else
+        //    {
+        //        app.UseExceptionHandler("/Home/Error");
+        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        //        app.UseHsts();
+        //    }
+        //    app.UseHttpsRedirection();
+        //    app.UseStaticFiles();
+
+        //    app.UseRouting();
+
+        //    app.UseAuthorization();
+
+        //    app.UseSwagger();
+
+        //    app.UseSwaggerUI(c =>
+        //    {
+        //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger eShopSolution V1");
+        //    });
+
+        //    app.UseEndpoints(endpoints =>
+        //    {
+        //        endpoints.MapControllerRoute(
+        //            name: "default",
+        //            pattern: "{controller=Home}/{action=Index}/{id?}");
+        //    });
+        //}
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
