d7f6ecca4e7732d79a6a34780147462ace02aa3d
LE TIN <tinleq95@outlook.com>
2020-11-19 07:58:31 +0700

    Add Authorization header

--- eShopSolution.BackendApi/Controllers/ProductsController.cs
+++ eShopSolution.BackendApi/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using eShopSolution.Application.Catalog.Products;
 using eShopSolution.ViewModel.Catalog.ProductImages;
 using eShopSolution.ViewModel.Catalog.Products;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,6 +14,7 @@ namespace eShopSolution.BackendApi.Controllers
     //api/products
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
