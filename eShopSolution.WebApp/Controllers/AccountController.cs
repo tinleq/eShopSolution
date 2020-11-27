aa024bcfdc0a857ccd074424cccedbb95e3c4584
LE TIN <tinleq95@outlook.com>
2020-11-27 16:44:23 +0700

    Register webportal

--- eShopSolution.WebApp/Controllers/AccountController.cs
+++ eShopSolution.WebApp/Controllers/AccountController.cs
@@ -40,12 +40,12 @@ namespace eShopSolution.WebApp.Controllers
         public async Task<IActionResult> Login(LoginRequest request)
         {
             if (!ModelState.IsValid)
-                return View(ModelState);
+                return View(request);
 
             var result = await _userApiClient.Authenticate(request);
             if (result.ResultObj == null)
             {
-                ModelState.AddModelError("", result.Message);
+                ModelState.AddModelError("", "Login failure");
                 return View();
             }
             var userPrincipal = this.ValidateToken(result.ResultObj);
@@ -71,21 +71,60 @@ namespace eShopSolution.WebApp.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterRequest registerRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerRequest);
+            }
+
+            var result = await _userApiClient.RegisterUser(registerRequest);
+            if (!result.IsSuccessed)
+            {
+                ModelState.AddModelError("", result.Message);
+                return View();
+            }
+            var loginResult = await _userApiClient.Authenticate(new LoginRequest()
+            {
+                UserName = registerRequest.UserName,
+                Password = registerRequest.Password,
+                RememberMe = true
+            });
+
+            var userPrincipal = this.ValidateToken(loginResult.ResultObj);
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = false
+            };
+            HttpContext.Session.SetString(SystemConstants.AppSettings.Token, loginResult.ResultObj);
+            await HttpContext.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        userPrincipal,
+                        authProperties);
+
+            return RedirectToAction("Index", "Home");
+        }
+
         private ClaimsPrincipal ValidateToken(string jwtToken)
         {
             IdentityModelEventSource.ShowPII = true;
-
             SecurityToken validatedToken;
             TokenValidationParameters validationParameters = new TokenValidationParameters();
 
             validationParameters.ValidateLifetime = true;
-
             validationParameters.ValidAudience = _configuration["Tokens:Issuer"];
             validationParameters.ValidIssuer = _configuration["Tokens:Issuer"];
             validationParameters.IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]));
 
             ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
-
             return principal;
         }
     }
