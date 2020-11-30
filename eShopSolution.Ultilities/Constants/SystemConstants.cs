026a0d7e0acef0c4f505f21ff40a576f23eb994f
LE TIN <tinleq95@outlook.com>
2020-11-30 10:15:53 +0700

    Add Cart item

--- eShopSolution.Ultilities/Constants/SystemConstants.cs
+++ eShopSolution.Ultilities/Constants/SystemConstants.cs
@@ -7,6 +7,7 @@ namespace eShopSolution.Ultilities.Constants
     public class SystemConstants
     {
         public const string MainConnectionString = "eShopSolutionDb";
+        public const string CartSession = "CartSession";
 
         public class AppSettings
         {
